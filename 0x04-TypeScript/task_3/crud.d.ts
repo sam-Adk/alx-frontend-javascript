8f29c058e87fd09c0e894b7cde38eb085eac21df
Samuel  adikah Mutesh <samsmollett50@gmail.com>
2025-09-15 00:59:12 +0300

    Update crud.d.ts

--- 0x04-TypeScript/task_3/crud.d.ts
+++ 0x04-TypeScript/task_3/crud.d.ts
@@ -1,7 +1,11 @@
-import { RowID, RowElement } from "./interface";
+
+// task_3/js/crud.d.ts
+import { RowID, RowElement } from './interface';
 
 export function insertRow(row: RowElement): RowID;
 export function deleteRow(rowId: RowID): void;
 export function updateRow(rowId: RowID, row: RowElement): RowID;
+
+// declare function insertRow(row: RowElement): number", "declare function deleteRow(rowId: RowID): void", "declare function updateRow(rowId: RowID, row: RowElement): RowID;
 
 
